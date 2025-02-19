5da557072a867a590671efcf682cdefb5e672617
zifeiyu <qiangren0613@gmail.com>
2025-02-19 21:25:43 +0800

    调整项目结构

---
+++ crates/common/src/lib.rs
@@ -0,0 +1,16 @@
+/// **通用工具 (日志, 配置, 错误)**
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
